26f1188a591275c6e592d7b8a7fb085d5b4edf2b
Eduardo Henrique <eduardo.hor@outlook.com>
2024-06-14 10:31:19 -0300

    :sparkles: feat:adding relationship between products and categories

--- motoca-systems/app/Models/Product.php
+++ motoca-systems/app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
@@ -15,4 +16,9 @@ class Product extends Model
         'descricao',
         'preco'
     ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
