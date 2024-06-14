c9aad3274f61e178729a0cfcbb2d3181498e4bc2
Eduardo Henrique <eduardo.hor@outlook.com>
2024-06-13 22:39:53 -0300

    :sparkles: feat:creating endpoint to create new category with validations

--- motoca-systems/app/Http/Controllers/CategoryController.php
+++ motoca-systems/app/Http/Controllers/CategoryController.php
@@ -2,9 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CategoryRequest;
+use App\Models\Category;
+use Exception;
+use GuzzleHttp\Psr7\Response;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
-    //
+    protected $category;
+
+    public function __construct(Category $category)
+    {
+        $this->category = $category;
+    }
+
+    public function store(CategoryRequest $request): JsonResponse
+    {
+        DB::beginTransaction();
+
+        try {
+            $category = $this->category->create([
+                'nome' => $request->nome
+            ]);
+
+            DB::commit();
+
+            return response()->json([
+                'status' => true,
+                'mensagem' => 'Categoria cadastrada com sucesso!',
+                'categoria' => $category
+            ], 201);
+        } catch (Exception $e) {
+            DB::rollBack();
+
+            return response()->json([
+                'status' => false,
+                'message' => 'Categoria não cadastrada'
+            ], 400);
+        }
+    }
 }
---
+++ motoca-systems/app/Http/Requests/CategoryRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+
+class CategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function failedValidation(Validator $validator): JsonResponse
+    {
+        throw new HttpResponseException(response()->json([
+            'status' => false,
+            'erros' => $validator->errors()
+        ], 422));
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nome' => 'required|min:3'
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+
+    public function messages(): array
+    {
+        return [
+            'nome.required' => 'O nome é obrigatório',
+            'nome.min' => 'O nome deve conter no mínimo 3 caracteres',
+        ];
+    }
+    }
